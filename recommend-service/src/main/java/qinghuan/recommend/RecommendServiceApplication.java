cf3876c930fb457b0770e9abf291194caa139e95
qinghuan11 <1926563663@qq.com>
2025-04-29 19:56:23 +0800

    初始化推荐服务

--- recommend-service/src/main/java/qinghuan/recommend/RecommendServiceApplication.java
+++ recommend-service/src/main/java/qinghuan/recommend/RecommendServiceApplication.java
@@ -3,9 +3,11 @@ package qinghuan.recommend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(basePackages = {"qinghuan.recommend", "qinghuan.common"})
 public class RecommendServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(RecommendServiceApplication.class, args);
