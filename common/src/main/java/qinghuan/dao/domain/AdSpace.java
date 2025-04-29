2ca2c421f4daf5490738c3c276cc84f4b5c673f1
qinghuan11 <1926563663@qq.com>
2025-04-29 19:56:19 +0800

    广告位实体

--- common/src/main/java/qinghuan/dao/domain/AdSpace.java
+++ common/src/main/java/qinghuan/dao/domain/AdSpace.java
@@ -1,4 +1,21 @@
 package qinghuan.dao.domain;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@TableName("t_ad_space")
 public class AdSpace {
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
