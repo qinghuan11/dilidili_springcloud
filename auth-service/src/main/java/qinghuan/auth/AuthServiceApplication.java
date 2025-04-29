310f4aa0557c5dc53ec5c251a1217abcb7d66732
qinghuan11 <1926563663@qq.com>
2025-04-29 19:56:17 +0800

    初始化认证服务

--- auth-service/src/main/java/qinghuan/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/qinghuan/auth/AuthServiceApplication.java
@@ -1,17 +1,20 @@
 package qinghuan.auth;
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 /**
  * 认证服务启动类
  */
 @SpringBootApplication
-@EnableDiscoveryClient
-@ComponentScan(basePackages = {"qinghuan.auth", "qinghuan.common"})
+//@EnableDiscoveryClient
+@MapperScan("qinghuan.auth.dao.mapper")
+//@ComponentScan(basePackages = {"qinghuan.auth", "qinghuan.common"})
 public class AuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
