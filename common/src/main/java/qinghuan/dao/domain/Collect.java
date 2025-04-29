c77f1b9d55df0aab2de571b55906a3378965ba8f
qinghuan11 <1926563663@qq.com>
2025-04-29 19:56:20 +0800

    收藏实体

--- common/src/main/java/qinghuan/dao/domain/Collect.java
+++ common/src/main/java/qinghuan/dao/domain/Collect.java
@@ -1,4 +1,18 @@
 package qinghuan.dao.domain;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@TableName("t_collect")
 public class Collect {
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    private Long userId;
+    private Long videoId;
+    private LocalDateTime createdAt;
 }
