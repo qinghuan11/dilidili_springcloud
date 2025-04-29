874d37e2f6ae36b7ca286ebd11aeff8e11230209
qinghuan11 <1926563663@qq.com>
2025-04-29 19:56:13 +0800

    初始化广告服务

--- ad-service/src/main/java/qinghuan/ad/AdServiceApplication.java
+++ ad-service/src/main/java/qinghuan/ad/AdServiceApplication.java
@@ -1,16 +1,17 @@
 package qinghuan.ad;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
-/**
- * API 网关服务启动类
- */
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ApiGatewayApplication {
+@MapperScan("qinghuan.ad.dao.mapper")
+@ComponentScan(basePackages = {"qinghuan.ad", "qinghuan.common"})
+public class AdServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayApplication.class, args);
+        SpringApplication.run(AdServiceApplication.class, args);
     }
 }
