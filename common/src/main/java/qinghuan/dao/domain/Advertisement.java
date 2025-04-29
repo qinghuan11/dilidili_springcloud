6e79b39878f5327608dcd9cdb9ae9de37ed1fee2
qinghuan11 <1926563663@qq.com>
2025-04-29 19:56:19 +0800

    广告实体

--- common/src/main/java/qinghuan/dao/domain/Advertisement.java
+++ common/src/main/java/qinghuan/dao/domain/Advertisement.java
@@ -1,4 +1,22 @@
 package qinghuan.dao.domain;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@TableName("t_advertisement")
 public class Advertisement {
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    private Long userId;
+    private Long adSpaceId;
+    private String filePath;
+    private String title;
+    private String status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
