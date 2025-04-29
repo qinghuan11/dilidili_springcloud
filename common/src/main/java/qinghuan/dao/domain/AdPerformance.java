b5ce78b8d320053f4d165793ad6ec8123bf739da
qinghuan11 <1926563663@qq.com>
2025-04-29 19:56:19 +0800

    广告性能实体

--- common/src/main/java/qinghuan/dao/domain/AdPerformance.java
+++ common/src/main/java/qinghuan/dao/domain/AdPerformance.java
@@ -1,4 +1,20 @@
 package qinghuan.dao.domain;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@TableName("t_ad_performance")
 public class AdPerformance {
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    private Long adId;
+    private Long impressions;
+    private Long clicks;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
