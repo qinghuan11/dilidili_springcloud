f5b1b0c3bcb88276da791509f781d2520f10ca86
qinghuan11 <1926563663@qq.com>
2025-04-29 19:56:16 +0800

    实现认证过滤

--- api-gateway/src/main/java/qinghuan/gateway/filter/AuthFilter.java
+++ api-gateway/src/main/java/qinghuan/gateway/filter/AuthFilter.java
@@ -1,6 +1,6 @@
 package qinghuan.gateway.filter;
 
-import qinghuan.common.JwtUtil;
+import qinghuan.gateway.config.JwtUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
