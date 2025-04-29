4c0eb2b2ca5ac4bc30d48c869748e7eb3bdde21c
qinghuan11 <1926563663@qq.com>
2025-04-29 19:56:14 +0800

    广告服务接口

--- ad-service/src/main/java/qinghuan/ad/service/AdService.java
+++ ad-service/src/main/java/qinghuan/ad/service/AdService.java
@@ -1,4 +1,15 @@
 package qinghuan.ad.service;
 
-public class AdService {
+import qinghuan.common.Result;
+import qinghuan.dao.domain.AdSpace;
+import qinghuan.dao.domain.Advertisement;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface AdService {
+    Result<String> buyAdSpace(Long adSpaceId, Long userId, MultipartFile file, String title);
+    Result<String> approveAd(Long adId, String status);
+    Result<String> recordImpression(Long adId);
+    Result<String> recordClick(Long adId);
+    Result<AdSpace> getAdSpace(Long adSpaceId);
+    Result<Advertisement> getAdvertisement(Long adId);
 }
